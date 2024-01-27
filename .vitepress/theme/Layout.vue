5575e67d220f06511575d55f5068fedabcb51308
JMaylor <60825814+JMaylor@users.noreply.github.com>
2024-01-27 11:37:35 +0000

    feat: home hero image

---
+++ .vitepress/theme/Layout.vue
@@ -0,0 +1,26 @@
+<script setup>
+import DefaultTheme from 'vitepress/theme'
+import LogoParallax from './LogoParallax.vue'
+</script>
+
+<template>
+  <DefaultTheme.Layout>
+    <template #home-hero-image>
+      <LogoParallax class="w-full h-full" />
+    </template>
+  </DefaultTheme.Layout>
+</template>
+
+<style>
+.VPHero .image-bg {
+  display: none;
+}
+
+.VPHero .image-container {
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  flex-shrink: 0;
+}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['components/**/*.vue'],
+  content: ['components/**/*.vue', '.vitepress/theme/**/*.vue'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '960px',
+    },
   },
   plugins: [],
 }
